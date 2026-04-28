8e81cd22a6addf7f16191f39eedcf86927c3db66
abezzi <abezzi@outlook.cl>
2026-04-28 15:31:11 -0400

    style(commands): underscore to unused arguments in the reset command

--- src/commands/commandReset.ts
+++ src/commands/commandReset.ts
@@ -1,7 +1,7 @@
 import { deleteUsers } from "src/db/queries/users";
 import type { CommandHandler } from "./types";
 
-export const commandReset: CommandHandler = async (_cmdName: string, ...args: string[]) => {
+export const commandReset: CommandHandler = async (_cmdName: string, ..._args: string[]) => {
   try {
     await deleteUsers();
     console.log(`All users deleted`);
